aa9c20b14b6f80d3a08ccec09bfd88586738d102
krkarma777 <krkarma777@gmail.com>
2024-04-12 00:59:23 +0900

    ChatRoomMemberDTO 추가

---
+++ src/main/java/com/chat/dto/chatRoomMembers/ChatRoomMembersDTO.java
@@ -0,0 +1,12 @@
+package com.chat.dto.chatRoomMembers;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatRoomMembersDTO {
+
+    private Long chatRoomId;
+    private String userId;
+}
